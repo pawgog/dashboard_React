94fb92530c603060db1b43629a686a5eeb376bde
pawgog <p.gogola@interia.pl>
2020-09-08 21:17:22 +0200

    Change fetching data url.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -37,7 +37,7 @@ class Dashboard extends React.Component {
   };
 
   componentDidMount() {
-    fetch('https://applicants324-user.herokuapp.com/applicants')
+    fetch('https://dashboard-applicants234.herokuapp.com/applicants')
       .then((response) => response.json())
       .then((data) =>
         this.setState({
